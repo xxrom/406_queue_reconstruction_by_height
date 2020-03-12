9f884e9c0102c2c5f1df167b62eb1c9222d6a160
nikita <xpom5566@mail.ru>
2020-03-12 22:48:19 +0300

    safe point 0

---
+++ main.py
@@ -0,0 +1,25 @@
+from typing import List
+
+# TODO: after array implementation
+# class Node:
+#   def __init__(self, x: int, next = None, prev = None):
+#     self.val = x
+#     self.next = next
+#     self.prev = prev
+
+class Solution:
+  def swap(self, array: List[List[int]], i: int, j: int):
+    # swap
+    return array
+
+  def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+
+
+
+my = Solution()
+
+n = [[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]
+
+ans = my.reconstructQueue(n)
+
+print('ans', ans)
